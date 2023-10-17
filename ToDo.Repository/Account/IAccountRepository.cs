65bf1657b2f9f25c9579aae9d0bd19d43679d877
Anthonia Okwe <anthonia_okwe@yahoo.com>
2023-10-17 23:24:28 +0100

    IDENTITY

---
+++ ToDo.Repository/Account/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ToDo.Models.Account;
+
+namespace ToDo.Repository.Account
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken);
+        public Task<ApplicationUserIdentity> GetbYUsernameAsync(string normalizedUsername, CancellationToken cancellationToken);
+    }
+}
